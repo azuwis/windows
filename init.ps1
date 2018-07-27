d295aa7da4e40d5e136265a4b1cd220c7e793e4b
Zhong Jianxin <azuwis@gmail.com>
2018-07-27 15:38:39 +0800

    init: Use json to serialize registry value

--- init.ps1
+++ init.ps1
@@ -58,10 +58,11 @@ function InstallUrl {
 
 function Registry {
     param($Path,$Name,$Value,$Type)
+    $Json = (ConvertTo-Json $Value)
     if (-not (Test-Path $Path)) {
-        RunAsAdmin "New-Item `"$Path`" -Force | New-ItemProperty -Name `"$Name`" -Value $Value -PropertyType `"$Type`" -Force; read-host"
-    } elseif (-not ((Get-ItemProperty $Path | Select-Object -ExpandProperty $Name -ErrorAction Ignore) -eq $Value)) {
-        RunAsAdmin "Set-ItemProperty `"$Path`" -Name `"$Name`" -Value $Value -Type `"$Type`" -Force"
+        RunAsAdmin "New-Item `"$Path`" -Force | New-ItemProperty -Name `"$Name`" -Value (ConvertFrom-Json `"$Json`") -PropertyType $Type -Force"
+    } elseif (-not ((ConvertTo-Json (Get-ItemProperty $Path | Select-Object -ExpandProperty $Name -ErrorAction Ignore)) -eq $Json)) {
+        RunAsAdmin "Set-ItemProperty `"$Path`" -Name `"$Name`" -Value (ConvertFrom-Json `"$Json`") -Type $Type -Force"
     }
 }
 
